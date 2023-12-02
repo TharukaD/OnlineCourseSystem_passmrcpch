45be0cd4eafbb6f48fe592d2a07a0888d5fa1e6e
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-12-02 19:02:32 +0500

    Added register page

--- OnlineCourseSystem/Controllers/HomeController.cs
+++ OnlineCourseSystem/Controllers/HomeController.cs
@@ -1,20 +1,37 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using OnlineCourseSystem.Services;
+using OnlineCourseSystem.ViewModels;
+using OnlineCourseSystem.ViewModels.Service;
 
 namespace OnlineCourseSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private IMapper _mapper;
         private readonly ILogger<HomeController> _logger;
+        private IServiceService _service;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            IMapper mapper,
+            ILogger<HomeController> logger,
+            IServiceService service
+        )
         {
+            _mapper = mapper;
             _logger = logger;
+            _service = service;
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var viewModel = new HomePageViewModel();
+
+            var services = await _service.GetAll();
+            viewModel.Services = _mapper.Map<List<ServiceViewModel>>(services);
+
+            return View(viewModel);
         }
 
         [HttpGet]
@@ -24,9 +41,32 @@ namespace OnlineCourseSystem.Controllers
         }
 
         [HttpGet]
-        public IActionResult Services()
+        public async Task<IActionResult> Services()
         {
-            return View();
+            var services = await _service.GetAll();
+            var servicesList = _mapper.Map<List<ServiceViewModel>>(services);
+            return View(servicesList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Service(int id)
+        {
+            try
+            {
+                var service = await _service.GetById(id);
+
+                if (service == null)
+                {
+                    return View("_ServiceNotFound");
+                }
+
+                var viewModel = _mapper.Map<ServiceViewModel>(service);
+                return View(viewModel);
+            }
+            catch (Exception ex)
+            {
+                return PartialView("_ServiceLoadingError");
+            }
         }
 
         [HttpGet]
---
+++ OnlineCourseSystem/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OnlineCourseSystem.ViewModels.Service;
+
+namespace OnlineCourseSystem.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public List<ServiceViewModel> Services { get; set; }
+        public HomePageViewModel()
+        {
+            Services = new List<ServiceViewModel>();
+        }
+    }
+}
